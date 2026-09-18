295959c9d366bd7db4680271075a5a18f6171086
cyanoge1-netizen <cyanoge1-netizen@users.noreply.github.com>
2026-09-18 14:56:07 +0600

    fix: resolve terminal scrolling via system clear and sanitize input buffer

--- boundary.c
+++ boundary.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
 int height=10,width=20,i,j;
 int x=10,y=5; //x represents column (left to right) and y represents Row (up to down)
 char move;
 //i represents height (row) , j represents column (width)
 while(1){
+//screen clearing
+system("clear");
 for(i=0;i<height;i++){
 	for(j=0;j<width;j++){
 		if(i==0 || i==height-1 || j==0 || j==width-1){
@@ -31,16 +34,27 @@ for(i=0;i<height;i++){
 
 
 }
-//input and state update
+//input and  buffer flash
+	
 	printf("Move (w/a/s/d, q to  exit): ");
-	scanf("%c",&move);
-	if(move == 'w')
+	if( scanf(" %c",&move) != 1){
+		break;
+}
+	//extra newline clear
+	while(getchar() != '\n');
+
+	// statement update & exit checking
+	if(move == 'q' || move == 'Q'){
+		printf("Exiting Game...\n");
+		break;
+}
+	else if(move == 'w'|| move == 'W')
 		y--; //going up
-	else if(move == 's')
+	else if(move == 's' || move == 'S')
 		y++; //going down
-	else if(move == 'd')
+	else if(move == 'd' || move == 'D')
 		x++; //going right
-	else if(move == 'a')
+	else if(move == 'a' || move == 'A')
 		x--; //going left
 
 
